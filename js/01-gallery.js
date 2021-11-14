92137a14ce2e12a116bc04266987fac0eddae934
Searge <ashikcygan@gmail.com>
2021-11-14 09:23:28 +0200

    Gallery One

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,46 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const documentGallery = document.querySelector('.gallery');
+const imageCard = onCreateGalleryItem(galleryItems);
+let openOriginalImage;
+
+documentGallery.insertAdjacentHTML('beforeend', imageCard);
+documentGallery.addEventListener('keydown', onPressEscToCloseImage);
+
+function onCreateGalleryItem(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+        <div class="gallery__item">
+             <a class="gallery__link" href="large-image.jpg">
+                <img
+                    class="gallery__image lazyload"
+                    data-src="${preview}"
+                    data-source="${original}"
+                    alt="${description}"
+                />
+            </a>
+        </div>
+        `;
+    })
+    .join('');
+}
+
+documentGallery.addEventListener('click', event => {
+  event.preventDefault();
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+  openOriginalImage = basicLightbox.create(`
+		<img src="${event.target.dataset.source}" width="1280" height="900">
+	`);
+  openOriginalImage.show();
+});
+
+function onPressEscToCloseImage(event) {
+  if (event.code !== 'Escape') {
+    return;
+  }
+  openOriginalImage.close();
+}
